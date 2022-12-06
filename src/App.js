4ca354d5348a426e45715004e10ea4299fae8a6b
AngomRanjan <angomranjan@gmail.com>
2022-12-07 00:42:56 +0530

    Add Freelancers List

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
+// import Navigation from './components/Navigation';
+import Freelancers from './components/Freelancers';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        {/* <Navigation /> */}
         <Routes>
-          <Route exact path="/" element={<Navigation />} />
+          <Route exact path="/" element={<Freelancers />} />
           <Route path="*" element={<h1 className="font-bold text-2xl text-center mt-20">404 Page Not Found</h1>} />
         </Routes>
       </BrowserRouter>
