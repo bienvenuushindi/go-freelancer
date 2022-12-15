ff3b8eb3eec999fe4921bf2302eeb3d9125c8e03
usbbush <jeanbienvenusb@gmail.com>
2022-12-15 12:23:19 +0200

    Connect to api-end point

--- src/components/AddFreelancer.js
+++ src/components/AddFreelancer.js
@@ -1,119 +1,156 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addFreelancerAction } from '../redux/freelancersReducer';
 
-const AddFreelancer = () => (
-  <>
-    {/* add from using tailwindcss */}
-    <form className="w-full  mt-40 flex justify-center">
-      <div className="w-6/12">
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="name"
-            >
-              Name
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="name"
-                type="text"
-                name="name"
-                placeholder="Name"
-                required
-                minLength="2"
-              />
-            </label>
-          </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="image"
-            >
-              Photo
-              <input
-                className="appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="image"
-                type="file"
-                placeholder="Image"
-                multiple
-                name="image"
-              />
-            </label>
+const AddFreelancer = () => {
+  const data = {
+    name: '',
+    fee: 0,
+    details: '',
+    photo: null,
+    location: '',
+  };
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState(data);
+  const handleChange = (e) => {
+    setFormData((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
+  };
+  const onImageChange = (e) => {
+    setFormData((prevState) => ({ ...prevState, photo: e.target.files[0] }));
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = new FormData();
+    form.append('name', formData.name);
+    form.append('fee', formData.fee);
+    form.append('details', formData.details);
+    form.append('location', formData.location);
+    form.append('photo', 'image-url');
+    form.append('featured_image', formData.photo);
+    console.log(formData);
+    dispatch(addFreelancerAction(form));
+  };
+  return (
+    <>
+      {/* add from using tailwindcss */}
+      <form className="w-full  mt-40 flex justify-center" onSubmit={(e) => handleSubmit(e)}>
+        <div className="w-6/12">
+          <div className="flex flex-wrap -mx-3 mb-2">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="name"
+              >
+                Name
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id="name"
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  required
+                  minLength="2"
+                  onChange={(e) => handleChange(e)}
+                />
+              </label>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="fee"
-            >
-              Fee
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="type"
-                type="number"
-                placeholder="fee"
-                name="fee"
-                required
-              />
-            </label>
-          </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="details"
-            >
-              Details
-              <textarea
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight  resize-none focus:outline-none focus:bg-white"
-                id="details"
-                rows="5"
-                name="details"
-                placeholder="Details"
-                required
-                minLength="5"
-              />
-            </label>
-          </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="location"
-            >
-              Location
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="location"
-                name="location"
-                type="text"
-                placeholder="Location"
-                required
-                minLength="2"
-              />
-            </label>
+          <div className="flex flex-wrap -mx-3 mb-2">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="image"
+              >
+                Photo
+                <input
+                  className="appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id="photo"
+                  type="file"
+                  placeholder="Photo"
+                  name="photo"
+                  accept="image/*"
+                  multiple={false}
+                  onChange={(e) => onImageChange(e)}
+                />
+              </label>
 
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-2">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="fee"
+              >
+                Fee
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id="type"
+                  type="number"
+                  placeholder="fee"
+                  name="fee"
+                  required
+                  onChange={(e) => handleChange(e)}
+                />
+              </label>
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-2">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="details"
+              >
+                Details
+                <textarea
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight  resize-none focus:outline-none focus:bg-white"
+                  id="details"
+                  rows="5"
+                  name="details"
+                  placeholder="Details"
+                  required
+                  minLength="5"
+                  onChange={(e) => handleChange(e)}
+                />
+              </label>
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-2">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="location"
+              >
+                Location
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id="location"
+                  name="location"
+                  type="text"
+                  placeholder="Location"
+                  required
+                  minLength="2"
+                  onChange={(e) => handleChange(e)}
+                />
+              </label>
+
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-2">
+            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <button
+                className="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="submit"
+              >
+                Add Car
+              </button>
+            </div>
           </div>
         </div>
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <button
-              className="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-              type="submit"
-            >
-              Add Car
-            </button>
-          </div>
-        </div>
-      </div>
 
-    </form>
-  </>
-);
-
+      </form>
+    </>
+  );
+};
 export default AddFreelancer;
--- src/redux/freelancersReducer.js
+++ src/redux/freelancersReducer.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import BaseUrl from './base_url';
 
 const GET_FREELANCERS = 'GET_FREELANCERS';
+const ADD_FREELANCERS = 'ADD_FREELANCERS';
 
 const initialState = [];
 
@@ -9,6 +10,9 @@ export const fetchFreelancers = (data) => ({
   type: GET_FREELANCERS, payload: data,
 });
 
+export const addFreelancer = (data) => ({
+  type: GET_FREELANCERS, payload: data,
+});
 export const getFreelancersAction = () => async (dispatch) => {
   await axios.get(`${BaseUrl}api/v1/freelancers`, { headers: { Authorization: localStorage.getItem('token') } })
     .then((res) => {
@@ -16,11 +20,27 @@ export const getFreelancersAction = () => async (dispatch) => {
     });
 };
 
+export const addFreelancerAction = (formData) => async (dispatch) => {
+  await fetch(`${BaseUrl}api/v1/freelancers`, {
+    method: 'POST',
+    body: { freelancer: formData },
+    headers: { Authorization: localStorage.getItem('token') },
+  }).then((res) => {
+    dispatch(addFreelancer(res.data));
+  }).catch((error) => console.log(error));
+  // eslint-disable-next-line max-len
+  // await axios.post(`${BaseUrl}api/v1/freelancers`, { headers: { Authorization: localStorage.getItem('token') } })
+  //     .then((res) => {
+  //       dispatch(fetchFreelancers(res.data));
+  //     });
+};
+
 const freelancersReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_FREELANCERS:
       return [...action.payload];
-
+    case ADD_FREELANCERS:
+      return action.payload;
     default:
       return state;
   }
