20924e297201365687c76475fc94604cf8a61352
usbbush <jeanbienvenusb@gmail.com>
2022-12-15 08:19:56 +0200

    Add HTML5 attributes validation

--- src/components/AddFreelancer.js
+++ src/components/AddFreelancer.js
@@ -3,121 +3,115 @@ import React from 'react';
 const AddFreelancer = () => (
   <>
     {/* add from using tailwindcss */}
-    <form className="w-full max-w-lg">
-      <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full px-3">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="name"
-          >
-            Name
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="name"
-              type="text"
-              name="name"
-              placeholder="Name"
-            />
-          </label>
+    <form className="w-full  mt-40 flex justify-center">
+      <div className="w-6/12">
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="name"
+            >
+              Name
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Name"
+                required
+                minLength="2"
+              />
+            </label>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full px-3">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="image"
-          >
-            Image
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="image"
-              type="file"
-              placeholder="Image"
-              multiple
-              name="image"
-            />
-          </label>
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="image"
+            >
+              Photo
+              <input
+                className="appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="image"
+                type="file"
+                placeholder="Image"
+                multiple
+                name="image"
+              />
+            </label>
 
+          </div>
         </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full px-3">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="type"
-          >
-            Type
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="type"
-              type="text"
-              placeholder="Type"
-              name="type"
-            />
-          </label>
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="fee"
+            >
+              Fee
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="type"
+                type="number"
+                placeholder="fee"
+                name="fee"
+                required
+              />
+            </label>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full px-3">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="description"
-          >
-            Description
-            <textarea
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight  resize-none focus:outline-none focus:bg-white"
-              id="description"
-              name="description"
-              placeholder="Description"
-            />
-          </label>
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="details"
+            >
+              Details
+              <textarea
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight  resize-none focus:outline-none focus:bg-white"
+                id="details"
+                rows="5"
+                name="details"
+                placeholder="Details"
+                required
+                minLength="5"
+              />
+            </label>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full px-3">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="brand"
-          >
-            Brand
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="brand"
-              name="brand"
-              type="text"
-              placeholder="Brand"
-            />
-          </label>
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="location"
+            >
+              Location
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="location"
+                name="location"
+                type="text"
+                placeholder="Location"
+                required
+                minLength="2"
+              />
+            </label>
 
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <button
+              className="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              Add Car
+            </button>
+          </div>
         </div>
       </div>
-      <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full px-3">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="daily_rate"
-          >
-            Daily Rate
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="daily_rate"
-              name="daily_rate"
-              type="text"
-              placeholder="Daily Rate"
-            />
-          </label>
-        </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <button
-            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-            type="submit"
-          >
-            Add Car
-          </button>
-        </div>
-      </div>
+
     </form>
   </>
 );
