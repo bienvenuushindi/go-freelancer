b517270360332154015d1d82b257dfce5044ac69
usbbush <jeanbienvenusb@gmail.com>
2022-12-15 21:30:24 +0200

    Remove dead code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Freelancers from './components/Freelancers';
-// import MainPage from './protected/MainPage';
-// import Login from './auth/Login';
-// import Signup from './auth/Signup';
 import MyRoutes from './components/MyRoutes';
 
 function App() {
