f0a9ed10b3cc92aa224007bd35c67d8827c16853
AngomRanjan <angomranjan@gmail.com>
2022-12-16 07:54:19 +0530

    Make Details Responsive

--- src/components/Freelancers.js
+++ src/components/Freelancers.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getFreelancersAction } from '../redux/freelancersReducer';
-import SpecializationList from './specializationList';
 
 const Freelancers = () => {
   const freelancers = useSelector((state) => state.freelancers);
@@ -29,12 +28,12 @@ const Freelancers = () => {
               {freelancers.map((fl) => (
                 <Link to={`/details/${fl.id}`} type="button" key={fl.id}>
                   <div className="border col-auto rounded shadow-lg px-4 mb-6 flex flex-col items-center justify-center mobileWidth mobilePadding mx-auto" key={fl.id}>
-                    <img src={fl.featured_image ? fl.featured_image['url'] : 'default.png'} alt={fl.name} className="w-60 h-60 rounded-full p-1 my-1 bg-slate-100 border-slate-700" />
+                    <img src={fl.photo ? fl.photo : fl.featured_image ? fl.featured_image['url'] : 'default.png'} alt={fl.name} className="w-60 h-60 rounded-full p-1 my-1 bg-slate-100 border-slate-700" />
                     <h2 className="uppercase font-bold my-1">{fl.name}</h2>
                     <hr className="border-t-2 border-dashed border-gray-200 w-48 my-2" />
                     <p className="text-gray-400 text-center my-2 w-full">{fl.details}</p>
                     <div className="flex items-center justify-center w-full m-3">
-                      <SpecializationList specializations={fl.specializations} />
+                      {fl.specializations.join(', ')}
                     </div>
                   </div>
                 </Link>
