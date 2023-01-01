abd0f6a5c340494ee4210bbc4bbc8ed29fff36f7
usbbush <jeanbienvenusb@gmail.com>
2023-01-01 14:51:02 +0200

    Implement toast for alert messages

--- src/auth/authHelpers.js
+++ src/auth/authHelpers.js
@@ -1,8 +1,25 @@
+import toast, { Toaster } from 'react-hot-toast';
+import { MdOutlineClose } from 'react-icons/md';
 import Freelancers from '../components/Freelancers';
+
+function showMessage() {
+  return toast.custom((t) => (
+
+    <div className="flex justify-between gap-4 items-baseline bg-clrPrime text-white shadow-black shadow rounded ">
+      <span className="p-2">You are not signed in!</span>
+      <button type="button" className="bg-red-600 text-white h-full font-bold p-2  focus:border-0 hover:border-0" onClick={() => toast.dismiss(t.id)}>
+        <MdOutlineClose />
+      </button>
+    </div>
+  ), { id: 'unique-notification', position: 'top-right' });
+}
 
 export const isLoggedIn = () => {
   const token = localStorage.getItem('token');
-  if (!token || token === '') return false;
+  if (!token || token === '') {
+    showMessage();
+    return false;
+  }
   return true;
 };
 
@@ -13,7 +30,9 @@ export const RequireAuth = ({ children }) => {
     children
   ) : (
     <>
-      <div className="relative top-24 text-red-500 z-10 text-lg text-right pr-6">You are not signed in</div>
+      <div className="relative top-24 text-red-500 z-10 text-lg text-right pr-6">
+        <Toaster />
+      </div>
       <Freelancers />
     </>
   );
